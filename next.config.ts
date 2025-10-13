a3bc4fe975253342395f0dfa3ca86edd8f78f1a0
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-10-13 10:29:38 +0530

    feat: env setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
+import '@/env/client';
+import '@/env/server';
+
 const nextConfig: NextConfig = {
-	/* config options here */
+	devIndicators: false,
 };
 
 export default nextConfig;
