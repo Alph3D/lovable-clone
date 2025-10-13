414f5d4fafc3dabe960101592c5e3f9b4cec16ac
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-10-13 10:23:57 +0530

    feat: eslint and prettier setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,29 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-import "./globals.css";
+import './globals.css';
 
 const inter = Inter({
-  subsets: ["latin"],
+	subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Lovable Clone",
-  description: "Lovable Clone",
+	description: 'Lovable Clone',
+	title: 'Lovable Clone',
 };
 
 const RootLayout = ({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) => {
-  return (
-    <html lang="en">
-      <body className={cn(inter.className, "antialiased")}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={cn(inter.className, 'antialiased')}>{children}</body>
+		</html>
+	);
 };
 
 export default RootLayout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 const HomePage = () => {
-  return (
-    <div>
-      <h1 className="text-4xl font-bold">Home Page</h1>
+	return (
+		<div>
+			<h1 className='text-4xl font-bold'>Home Page</h1>
 
-      <Button variant="destructive" size="lg">
-        Click me
-      </Button>
-    </div>
-  );
+			<Button variant='destructive' size='lg'>
+				Click me
+			</Button>
+		</div>
+	);
 };
 
 export default HomePage;
