9626cc2c93af24244cabd800853ef407d49e7534
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-10-23 11:39:10 +0530

    feat: generate e2b sandbox url

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,23 +1,40 @@
+import { Sandbox } from '@e2b/code-interpreter';
 import { createAgent, openai } from '@inngest/agent-kit';
 
 import { env } from '@/env/server';
 
 import { inngest } from './client';
+import { getSandbox } from './utils';
 
 export const helloWorld = inngest.createFunction(
 	{ id: 'hello-world' },
 	{ event: 'test/hello.world' },
 	async ({ event, step }) => {
-		await step.sleep('wait-a-moment', '5s');
+		const sandboxId = await step.run('get-sandbox-id', async () => {
+			const sandbox = await Sandbox.create('vibe-nextjs-test-xxxx');
 
-		const summarizer = createAgent({
-			model: openai({ apiKey: env.OPENAI_API_KEY, model: 'gpt-4o' }),
-			name: 'summarizer',
-			system: 'You are an expert summarizer.  You summarize in 3 words or less.',
+			return sandbox.sandboxId;
 		});
 
-		const { output } = await summarizer.run(`Summarize the following text: ${event.data.value}`);
+		await step.sleep('wait-a-moment', '5s');
 
-		return { output };
+		const codeAgent = createAgent({
+			model: openai({ apiKey: env.OPENAI_API_KEY, model: 'gpt-4o' }),
+			name: 'code-agent',
+			system:
+				'You are an expert Next.js developer.  You write readable, maintainable code. You write simple Next.js & React snippets.',
+		});
+
+		const { output } = await codeAgent.run(`Write the following snippet: ${event.data.value}`);
+
+		const sandboxUrl = await step.run('get-sandbox-url', async () => {
+			const sandbox = await getSandbox(sandboxId);
+
+			const host = sandbox.getHost(3000);
+
+			return `https://${host}`;
+		});
+
+		return { output, sandboxUrl };
 	}
 );
