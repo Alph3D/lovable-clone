4c71b152015e59ffbbabc896f4bf96a669e0cef0
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-10-23 22:37:51 +0530

    feat: add toaster

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -2,6 +2,14 @@ import type { PropsWithChildren } from 'react';
 
 import { TRPCReactProvider } from '@/trpc/client';
 
+import { ToasterProvider } from './toaster-provider';
+
 export const Providers = ({ children }: Readonly<PropsWithChildren>) => {
-	return <TRPCReactProvider>{children}</TRPCReactProvider>;
+	return (
+		<TRPCReactProvider>
+			{children}
+
+			<ToasterProvider />
+		</TRPCReactProvider>
+	);
 };
