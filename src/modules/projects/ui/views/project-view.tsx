d60ba945d7ef2849d1e8fe4c345c5e64c5d281f7
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-11-12 23:11:34 +0530

    refactor(ui): add tabs components

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -1,12 +1,17 @@
 'use client';
 
 import { Suspense, useState } from 'react';
+import Link from 'next/link';
+
+import { CodeIcon, CrownIcon, EyeIcon } from 'lucide-react';
 
 import { FragmentWeb } from '@/modules/projects/ui/components/fragment-web';
 import { MessagesContainer } from '@/modules/projects/ui/components/messages-container';
 import { ProjectHeader } from '@/modules/projects/ui/components/project-header';
 
+import { Button } from '@/components/ui/button';
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import type { Fragment } from '@/generated/prisma';
 
 interface ProjectViewProps {
@@ -15,6 +20,7 @@ interface ProjectViewProps {
 
 export const ProjectView = ({ projectId }: ProjectViewProps) => {
 	const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
+	const [tabState, setTabState] = useState<'preview' | 'code'>('preview');
 
 	return (
 		<div className='h-screen'>
@@ -36,7 +42,40 @@ export const ProjectView = ({ projectId }: ProjectViewProps) => {
 				<ResizableHandle withHandle />
 
 				<ResizablePanel defaultSize={65} minSize={50}>
-					{!!activeFragment && <FragmentWeb data={activeFragment} />}
+					<Tabs
+						className='h-full gap-y-0'
+						defaultValue='preview'
+						value={tabState}
+						onValueChange={(value) => setTabState(value as 'preview' | 'code')}
+					>
+						<div className='flex w-full items-center gap-x-2 border-b p-2'>
+							<TabsList className='h-8 rounded-md border p-0'>
+								<TabsTrigger value='preview' className='rounded-md'>
+									<EyeIcon />
+									<span>Demo</span>
+								</TabsTrigger>
+
+								<TabsTrigger value='code' className='rounded-md'>
+									<CodeIcon />
+									<span>Code</span>
+								</TabsTrigger>
+							</TabsList>
+
+							<div className='ml-auto flex items-center gap-x-2'>
+								<Button size='sm' asChild>
+									<Link href='/pricing'>
+										<CrownIcon /> Upgrade
+									</Link>
+								</Button>
+							</div>
+						</div>
+
+						<TabsContent value='preview'>{!!activeFragment && <FragmentWeb data={activeFragment} />}</TabsContent>
+
+						<TabsContent value='code'>
+							<p>TODO: Code</p>
+						</TabsContent>
+					</Tabs>
 				</ResizablePanel>
 			</ResizablePanelGroup>
 		</div>
