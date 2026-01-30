408bcec5d9146743b48e33e34c85844716aa5666
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2026-01-30 14:20:49 +0530

    refactor: format files using prettier

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,11 +4,11 @@ import { defineConfig, env } from 'prisma/config';
 dotenv.config({ path: '.env.local' });
 
 export default defineConfig({
-	schema: 'prisma/schema.prisma',
-	migrations: {
-		path: 'prisma/migrations',
-	},
 	datasource: {
 		url: env('DATABASE_URL'),
 	},
+	migrations: {
+		path: 'prisma/migrations',
+	},
+	schema: 'prisma/schema.prisma',
 });
