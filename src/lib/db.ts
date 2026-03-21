16010151bbd7af371904b164bfa3ceaed10aff98
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2026-03-21 14:39:30 +0530

    fix: build errors

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,12 @@
 import { PrismaPg } from '@prisma/adapter-pg';
-import { attachDatabasePool } from '@vercel/functions';
-import { Pool } from 'pg';
 
 import { env } from '@/env/server';
 import { PrismaClient } from '@/generated/prisma/client';
 
-const pool = new Pool({ connectionString: env.DATABASE_URL });
-
-attachDatabasePool(pool);
-
 const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter: new PrismaPg(pool) });
+const prisma =
+	globalForPrisma.prisma ?? new PrismaClient({ adapter: new PrismaPg({ connectionString: env.DATABASE_URL }) });
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export const db = prisma;
